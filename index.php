e2f7b14bf93c0689bc6a54c6d35e767eb34ea3cd
akkramm00 <127404065+akkramm00@users.noreply.github.com>
2023-08-01 05:58:04 +0000

    Initial commit

---
+++ index.php
@@ -0,0 +1,24 @@
+<html>
+  <head>
+    <title>PHP Test</title>
+  </head>
+  <body>
+    <h2>Exercice sur  L' encapsulation en PHP</h2>
+     enoncé ::
+    Vous etes developpeur web au sein d'une équipe de 10 personnes et devez réaliser une application de e-commerce pour l'entreprise BigBroZer en vue de permettre le fichage de personnes .
+    Supposons que nous ayons une classe "Client" qui stocke des informations sur les clients d'une entreprise. La classe contient les propriétés "nom", "adresse", "email", et "numero de telephone".
+    Noius voulons utiliser l'encapsulation pour protéger et permettre l'accès à ces propriétés uniquement via des méthodes publiques.
+
+    Question:
+    Ecrivez la classe "Client" en respectant les principes de visibilité et d'encapsulation, puis écrivez le code permettant d'instancier un objet de la classe "Client" avec les informations de votre choix et d'afficher ces informations, il vous est impossible d'utiliser uniquement les méthodes magiques.
+
+    <h2>Solution</h2>
+
+    <?php
+class Client {
+  
+}
+
+?>
+  </body>
+</html>
